2c90a0e8359ddca159ad924f8deb393552f88a8e
Soham Mondal <soham.mondal@gmail.com>
2016-01-19 16:17:38 +0530

    Updated DeviceModelUtils to return just the model

--- Library/src/main/java/cc/soham/toggle/DeviceModelUtils.java
+++ Library/src/main/java/cc/soham/toggle/DeviceModelUtils.java
@@ -7,14 +7,16 @@ import android.text.TextUtils;
  * Created by sohammondal on 19/01/16.
  */
 public class DeviceModelUtils {
-    /** Returns the consumer friendly device name */
-    public static String getModel() {
-        String manufacturer = Build.MANUFACTURER;
-        String model = Build.MODEL;
+    /**
+     * Returns the consumer friendly device name
+     */
+    public static String getModel(String manufacturer, String model) {
+        if (manufacturer == null)
+            return model;
         if (model.startsWith(manufacturer)) {
             return model.substring(manufacturer.length());
         }
-        return capitalize(manufacturer) + " " + model;
+        return model;
     }
 
     private static String capitalize(String str) {
